78f4ac6d37a84a51fc29c9b7b5cfa8512e246a37
Jasiuka <luksjasiukaitis@gmail.com>
2024-07-10 20:44:54 +0300

    it works, removing console.log

--- src/utils/changeTheme.util.ts
+++ src/utils/changeTheme.util.ts
@@ -1,7 +1,6 @@
 import { Themes } from "@/types"
 export function changeTheme(newTheme: Themes) {
     const themeFile = document.getElementById("theme-link")
-    console.log(themeFile)
 
     if (newTheme === "light") {
         themeFile?.setAttribute("href", "/themes/light.css")
