6971af80ca4de1dd903215e6e1204cadf7d88029
Jasiuka <luksjasiukaitis@gmail.com>
2024-07-10 19:39:27 +0300

    theme change should work when hosted

--- src/utils/changeTheme.util.ts
+++ src/utils/changeTheme.util.ts
@@ -3,8 +3,8 @@ export function changeTheme(newTheme: Themes) {
     const themeFile = document.getElementById("theme-link")
 
     if (newTheme === "light") {
-        themeFile?.setAttribute("href", "/src/assets/styles/themes/light.css")
+        themeFile?.setAttribute("href", "./themes/light.css")
     } else {
-        themeFile?.setAttribute("href", "/src/assets/styles/themes/dark.css")
+        themeFile?.setAttribute("href", "./themes//dark.css")
     }
 }
